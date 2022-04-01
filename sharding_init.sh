d5a01a6a1d7340a80e8dba42fda24e8bc3ee4c55
xinyuan <516671327@qq.com>
2022-04-01 16:05:48 +0800

    Update sharding_init.sh

--- sharding_init.sh
+++ sharding_init.sh
@@ -1,35 +1,36 @@
 
-mongo $ip1 --port 27019
+##########shading initiate; 3 ips as parameters
+mongo $0 --port 27019
 rs.initiate(
   {
     _id: "shard1",
     members: [
-      { _id : 0, host : "$ip1:27019" },
-      { _id : 1, host : "$ip2:27019" },
-      { _id : 2, host : "$ip3:27019" }
+      { _id : 0, host : "$0:27019" },
+      { _id : 1, host : "$1:27019" },
+      { _id : 2, host : "$2:27019" }
     ]
   }
 )
-mongo $ip2 --port 27020
+mongo $1 --port 27020
 rs.initiate(
   {
     _id: "shard2",
     members: [
-      { _id : 0, host : "$ip1:27020" },
-      { _id : 1, host : "$ip2:27020" },
-      { _id : 2, host : "$ip3:27020" }
+      { _id : 0, host : "$0:27020" },
+      { _id : 1, host : "$1:27020" },
+      { _id : 2, host : "$2:27020" }
     ]
   }
 )
 
-mongo $ip3 --port 27021
+mongo $2 --port 27021
 rs.initiate(
   {
     _id: "shard3",
     members: [
-      { _id : 0, host : "$ip1:27021" },
-      { _id : 1, host : "$ip2:27021" },
-      { _id : 2, host : "$ip3:27021" }
+      { _id : 0, host : "$0:27021" },
+      { _id : 1, host : "$1:27021" },
+      { _id : 2, host : "$2:27021" }
     ]
   }
 )
