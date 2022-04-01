a101964baf8553504c79501bad7bca2aae04c431
xinyuan <516671327@qq.com>
2022-04-01 17:30:01 +0800

    Update sharding_init.sh

--- sharding_init.sh
+++ sharding_init.sh
@@ -1,3 +1,15 @@
+#######config server init
+rs.initiate(
+  {
+    _id: "config",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "172.31.77.178:27018" },
+      { _id : 1, host : "172.31.64.154:27018" },
+      { _id : 2, host : "172.31.68.172:27018" }
+    ]
+  }
+)
 
 ##########shading initiate; 3 ips as parameters
 mongo $0 --port 27019
@@ -5,9 +17,9 @@ rs.initiate(
   {
     _id: "shard1",
     members: [
-      { _id : 0, host : "$0:27019" },
-      { _id : 1, host : "$1:27019" },
-      { _id : 2, host : "$2:27019" }
+      { _id : 0, host : "172.31.77.178:27019" },
+      { _id : 1, host : "172.31.64.154:27019" },
+      { _id : 2, host : "172.31.68.172:27019" }
     ]
   }
 )
@@ -16,9 +28,9 @@ rs.initiate(
   {
     _id: "shard2",
     members: [
-      { _id : 0, host : "$0:27020" },
-      { _id : 1, host : "$1:27020" },
-      { _id : 2, host : "$2:27020" }
+      { _id : 0, host : "172.31.77.178:27020" },
+      { _id : 1, host : "172.31.64.154:27020" },
+      { _id : 2, host : "172.31.68.172:27020" }
     ]
   }
 )
@@ -28,9 +40,9 @@ rs.initiate(
   {
     _id: "shard3",
     members: [
-      { _id : 0, host : "$0:27021" },
-      { _id : 1, host : "$1:27021" },
-      { _id : 2, host : "$2:27021" }
+      { _id : 0, host : "172.31.77.178:27021" },
+      { _id : 1, host : "172.31.64.154:27021" },
+      { _id : 2, host : "172.31.68.172:27021" }
     ]
   }
 )
