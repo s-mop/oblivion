b34c8a26fb93309505ac78ff0930597c82119834
xinyuan <516671327@qq.com>
2022-04-01 17:13:41 +0800

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/install.sh)"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/node_setup.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/mongs_setup.sh)"
