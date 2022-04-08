d72474dcfc18ba5210df0eb9b488d6109c193d96
xinyuan <516671327@qq.com>
2022-04-08 17:47:19 +0800

    Update local_poc.cmd

--- local_poc.cmd
+++ local_poc.cmd
@@ -3,4 +3,11 @@ for (i = 1; i <= 20000; i=i+1){
     db.order.insert({'orderType': 2})
 }
 
+db.getCollection("order").createIndex({
+    userId: "hashed",
+    year: NumberInt("1")
+}, {
+    name: "cidx_order"
+}) 
+#need root auth and index
 sh.shardCollection("poc.order", {"userId": "hashed", "year": 1 })
