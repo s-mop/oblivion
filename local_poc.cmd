4c2040c3b115f1d199c27d5b7c76c3d2af4c9722
xinyuan <516671327@qq.com>
2022-04-08 16:42:51 +0800

    Update local_poc.cmd

--- local_poc.cmd
+++ local_poc.cmd
@@ -2,3 +2,5 @@ sh.shardCollection("poc.order", {"_id": "hashed" })
 for (i = 1; i <= 20000; i=i+1){
     db.order.insert({'orderType': 2})
 }
+
+sh.shardCollection("poc.order", {"userId": "hashed", "year": 1 })
