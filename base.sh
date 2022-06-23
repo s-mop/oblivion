1a59fe087a05d83e40c04823ce399e24a1c14f5d
xinyuan <516671327@qq.com>
2022-06-23 19:02:27 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,3 +2,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/instal
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/node_setup.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/s-mop/oblivion/main/mongs_setup.sh)"
+
+bash mongs_setup.sh 172.17.54.2 172.17.54.3 172.17.54.6
